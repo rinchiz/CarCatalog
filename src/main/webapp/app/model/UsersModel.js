a03f541e448ceac4b067d1d6f381d12c8a820b80
rinchiz <khamitovrinat115@gmail.com>
2019-08-19 19:30:39 +0600

    Commit user model

---
+++ src/main/webapp/app/model/UsersModel.js
@@ -0,0 +1,29 @@
+Ext.define('CarCatalog.model.UsersModel', {
+    extend: 'Ext.data.Model',
+    alias: 'widget.usersModels',
+    idProperty: 'userID',
+
+    fields: [{
+        name: 'userID',
+        type: 'int',
+    }, {
+        name: 'name',
+        type: 'string',
+        id: 'name'
+
+    }, {
+        name: 'surname',
+        type: 'string',
+        id: 'surname'
+    }, {
+        name: 'date',
+        type: 'date'
+    }, {
+        name: 'email',
+        type: 'string'
+    }, {
+        name: 'married',
+        type: 'bool'
+    }]
+});
+
